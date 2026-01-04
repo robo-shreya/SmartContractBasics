f10bac5da474b3c5818bd40a17d9e6341da05a90
Shreya <srivastava.shreya987@gmail.com>
2026-01-05 00:20:36 +0100

    interacting with imported contracts

--- src/StorageFactory.sol
+++ src/StorageFactory.sol
@@ -8,10 +8,31 @@ contract StorageFactory {
 
     //variables - "type visibility name"
 
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
+    // creating a list which will keep a record of addresses of already deployed SimpleStorage
+    // instead of deploying a new SimpleStorage directly
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(
+            newSimpleStorage
+        );
     }
-    
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // in order to interact with other contracts, we need:
+        // 1. Address 2. Function selector 
+
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(
+            _simpleStorageNumber
+        );
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+
 }
